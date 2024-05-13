f7ee9e9b472ab79c7008afb49a80805a60322366
BellyCarina <164196921+BellyCarina@users.noreply.github.com>
2024-05-13 11:30:37 -0300

    Add files via upload

---
+++ aula.java
@@ -0,0 +1,26 @@
+import  java.util.*;
+public class Atividade01 {
+    
+    //Atributos
+    public String nome , sobrenome;
+    public int idade, anoNascimento;
+    public double peso , altura;
+       
+     //Constante 
+    public static final String CPF= "05954155550";
+
+     
+     public static void main(String args[]){
+b   //Input
+
+    Scanner teclado= new Scanner(System.in)
+
+    //Output
+
+    System.out.printIn("Digite seu nome :");
+    nome = teclado.nexline();
+
+    System.out.printIn("Meu nome é:" + nome);
+
+}
+}
